8433e0069f608e34f67a01c0710fb4cd864085d1
John Yang <john.yang20@berkeley.edu>
2023-11-07 18:31:56 -0500

    `get_environment_yml` falls back on `base_commit`

--- harness/utils.py
+++ harness/utils.py
@@ -61,9 +61,11 @@ def get_environment_yml(instance: Dict, env_name: str, save_path: str = None) ->
     """
     # Attempt to find environment.yml at each path based on task instance's repo
     path_worked = False
+
+    commit = 'environment_setup_commit' if 'environment_setup_commit' in instance else 'base_commit'
     for req_path in MAP_REPO_TO_ENV_YML_PATHS[instance["repo"]]:
         reqs_url = os.path.join(
-            SWE_BENCH_URL_RAW, instance["repo"], instance['environment_setup_commit'], req_path
+            SWE_BENCH_URL_RAW, instance["repo"], instance[commit], req_path
         )
         reqs = requests.get(reqs_url)
         if reqs.status_code == 200:
