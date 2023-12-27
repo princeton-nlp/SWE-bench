adee40edd191f3513b14afe87bcd0901782c14e1
John Yang <john.yang20@berkeley.edu>
2023-12-27 10:21:07 -0800

    Add minor docstring to collection

--- collect/run_get_tasks_pipeline.sh
+++ collect/run_get_tasks_pipeline.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# If you'd like to parallelize, do the following:
+# * Create a .env file in this folder
+# * Declare GITHUB_TOKENS=token1,token2,token3...
 
 python get_tasks_pipeline.py \
     --repos 'scikit-learn/scikit-learn', 'pallets/flask' \
