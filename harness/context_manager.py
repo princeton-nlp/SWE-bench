e69b15b506435b050c4a49537aa0596fc324e494
Jason Gross <jgross@mit.edu>
2023-12-26 21:38:42 -0800

    Use the more portable `.` instead of `source`
    This avoids errors such as `/bin/sh: 1: source: not found`
    
    ```
    $ python harness/run_evaluation.py     --predictions_path example-predictions.json  --swe_bench_tasks example.json     --log_dir example-log/     --testbed example-testbed/
    Error: Command 'source /home/jgross/Documents/GitHub/SWE-bench/example-testbed/fakemodel/sqlfluff__sqlfluff/1.4/tmpdx4urtao/miniconda3/bin/activate sqlfluff__sqlfluff__1.4 && echo 'activate successful' && pip install -r /home/jgross/Documents/GitHub/SWE-bench/example-testbed/fakemodel/sqlfluff__sqlfluff/1.4/tmpfnim3stm/requirements.txt' returned non-zero exit status 127.
    Error stdout:
    Error stderr: /bin/sh: 1: source: not found
    
    Error traceback: Traceback (most recent call last):
      File "/home/jgross/Documents/GitHub/SWE-bench/harness/context_manager.py", line 50, in __call__
        output = subprocess.run(cmd, **combined_args)
      File "/home/jgross/.local64/mambaforge/envs/swe-bench/lib/python3.9/subprocess.py", line 528, in run
        raise CalledProcessError(retcode, process.args,
    subprocess.CalledProcessError: Command 'source /home/jgross/Documents/GitHub/SWE-bench/example-testbed/fakemodel/sqlfluff__sqlfluff/1.4/tmpdx4urtao/miniconda3/bin/activate sqlfluff__sqlfluff__1.4 && echo 'activate successful' && pip install -r /home/jgross/Documents/GitHub/SWE-bench/example-testbed/fakemodel/sqlfluff__sqlfluff/1.4/tmpfnim3stm/requirements.txt' returned non-zero exit status 127.
    
    Traceback (most recent call last):
      File "/home/jgross/Documents/GitHub/SWE-bench/harness/run_evaluation.py", line 186, in <module>
        main(**vars(args))
      File "/home/jgross/Documents/GitHub/SWE-bench/harness/run_evaluation.py", line 162, in main
        eval_engine(args)
      File "/home/jgross/Documents/GitHub/SWE-bench/harness/engine_evaluation.py", line 164, in main
        setup_testbed(data_groups[0])
      File "/home/jgross/Documents/GitHub/SWE-bench/harness/engine_validation.py", line 89, in setup_testbed
        with TestbedContextManager(
      File "/home/jgross/Documents/GitHub/SWE-bench/harness/context_manager.py", line 288, in __enter__
        self.exec(cmd, shell=True)
      File "/home/jgross/Documents/GitHub/SWE-bench/harness/context_manager.py", line 58, in __call__
        raise e
      File "/home/jgross/Documents/GitHub/SWE-bench/harness/context_manager.py", line 50, in __call__
        output = subprocess.run(cmd, **combined_args)
      File "/home/jgross/.local64/mambaforge/envs/swe-bench/lib/python3.9/subprocess.py", line 528, in run
        raise CalledProcessError(retcode, process.args,
    subprocess.CalledProcessError: Command 'source /home/jgross/Documents/GitHub/SWE-bench/example-testbed/fakemodel/sqlfluff__sqlfluff/1.4/tmpdx4urtao/miniconda3/bin/activate sqlfluff__sqlfluff__1.4 && echo 'activate successful' && pip install -r /home/jgross/Documents/GitHub/SWE-bench/example-testbed/fakemodel/sqlfluff__sqlfluff/1.4/tmpfnim3stm/requirements.txt' returned non-zero exit status 127.
    ```

--- harness/context_manager.py
+++ harness/context_manager.py
@@ -281,7 +281,7 @@ class TestbedContextManager:
 
                     # Install dependencies
                     path_to_reqs = get_requirements(setup_ref_instance, self.testbed)
-                    cmd = f"source {path_activate} {env_name} && echo 'activate successful' && pip install -r {path_to_reqs}"
+                    cmd = f". {path_activate} {env_name} && echo 'activate successful' && pip install -r {path_to_reqs}"
                     logger_testbed.info(
                         f"[Testbed] Installing dependencies for {env_name}; Command: {cmd}"
                     )
@@ -326,7 +326,7 @@ class TestbedContextManager:
 
                 # Install additional packages if specified
                 if "pip_packages" in install:
-                    cmd = f"source {path_activate} {env_name} && pip install {install['pip_packages']}"
+                    cmd = f". {path_activate} {env_name} && pip install {install['pip_packages']}"
                     logger_testbed.info(
                         f"[Testbed] Installing pip packages for {env_name}; Command: {cmd}"
                     )
@@ -424,7 +424,7 @@ class TaskEnvContextManager:
             self.log_file = os.path.join(
                 log_dir, f"{instance[KEY_INSTANCE_ID]}.{instance[KEY_MODEL]}.eval.log"
             )
-        self.cmd_activate = f"source {os.path.join(self.conda_path, 'bin', 'activate')} {self.venv} && echo 'activate successful'"
+        self.cmd_activate = f". {os.path.join(self.conda_path, 'bin', 'activate')} {self.venv} && echo 'activate successful'"
         self.timeout = timeout
         self.cwd = os.getcwd()
 
