1339903579442f807c612e7cbeae814fcded5a13
Carlos! <cjsaltlake@gmail.com>
2024-06-26 18:40:46 +0000

    Update predictions path arg description

--- swebench/harness/run_evaluation.py
+++ swebench/harness/run_evaluation.py
@@ -490,7 +490,7 @@ if __name__ == "__main__":
     parser.add_argument("--dataset_name", default="princeton-nlp/SWE-bench_Lite", type=str, help="name of dataset")
     parser.add_argument("--split", type=str, default="test")
     parser.add_argument("--instance_ids", nargs="+", type=str, help="Instance IDs to run (space separated)")
-    parser.add_argument("--predictions_path", type=str, help="Path to predictions file")
+    parser.add_argument("--predictions_path", type=str, help="Path to predictions file - if 'gold', uses gold predictions")
     parser.add_argument("--max_workers", type=int, default=4, help="Maximum number of workers (should be <= 75% of CPU cores)")
     parser.add_argument("--open_file_limit", type=int, default=4096, help="Open file limit")
     parser.add_argument("--timeout", type=int, default=1_800, help="Timeout (in seconds) for running tests for each instance")
