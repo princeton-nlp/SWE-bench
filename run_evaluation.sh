85815dad308c24c1139afd163b71e2abec32e008
Carlos! <cjsaltlake@gmail.com>
2024-06-26 22:06:20 +0000

    Update run_evaluation

--- run_evaluation.sh
+++ run_evaluation.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-python swebench/harness/run_evaluation.py \
-    --predictions_path preds_sbl.json \
-    --max_workers 8 \
-    --cache instance \
-    --instance_ids astropy__astropy-7746 django__django-13447 django__django-16046 psf__requests-863 scikit-learn__scikit-learn-25638 sympy__sympy-16106 django__django-10914 django__django-13448 django__django-16139 pydata__xarray-3364 scikit-learn__scikit-learn-25747 sympy__sympy-16281
+python -m swebench.harness.run_evaluation --predictions_path gold --max_workers 12 --cache env --run_id gold_verify
--- swebench/harness/run_evaluation.py
+++ swebench/harness/run_evaluation.py
@@ -496,18 +496,20 @@ if __name__ == "__main__":
     parser.add_argument("--dataset_name", default="princeton-nlp/SWE-bench_Lite", type=str, help="name of dataset")
     parser.add_argument("--split", type=str, default="test")
     parser.add_argument("--instance_ids", nargs="+", type=str, help="Instance IDs to run (space separated)")
-    parser.add_argument("--predictions_path", type=str, help="Path to predictions file - if 'gold', uses gold predictions")
-    parser.add_argument("--max_workers", type=int, default=4, help="Maximum number of workers (should be <= 75%% of CPU cores)")
+    parser.add_argument("--predictions_path", type=str, help="Path to predictions file - if 'gold', uses gold predictions", required=True)
+    parser.add_argument("--max_workers", type=int, default=4, help="Maximum number of workers (should be <= 75% of CPU cores)")
     parser.add_argument("--open_file_limit", type=int, default=4096, help="Open file limit")
-    parser.add_argument("--timeout", type=int, default=1_800, help="Timeout (in seconds) for running tests for each instance")
     parser.add_argument(
-        "--force_rebuild", type=str2bool, default=False, help="Force rebuild of images"
+        "--timeout", type=int, default=1_800, help="Timeout (in seconds) for running tests for each instance"
+        )
+    parser.add_argument(
+        "--force_rebuild", type=str2bool, default=False, help="Force rebuild of all images"
     )
     parser.add_argument(
         "--cache_level",
         type=str,
         choices=["none", "base", "env", "instance"],
-        help="Cache level",
+        help="Cache level - remove images above this level",
         default="env",
     )
     # if clean is true then we remove all images that are above the cache level
