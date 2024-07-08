be358d5bdaa43649fab52f0aae267e43316d59a2
John Yang <byjohnyang@gmail.com>
2024-07-08 11:37:02 -0400

    Fix tests

--- tests/test_collect_cli.py
+++ tests/test_collect_cli.py
@@ -10,7 +10,7 @@ def test_collect_smoke_test():
 
 
 def test_collect_one(tmp_path):
-    cmd = ["python", "-m", "swebench.collect.print_pulls", "pvlib/pvlib-python", str(tmp_path/ "out.txt"), "--max-pulls", "1"]
+    cmd = ["python", "-m", "swebench.collect.print_pulls", "pvlib/pvlib-python", str(tmp_path/ "out.txt"), "--max_pulls", "1"]
     print(" ".join(cmd))
     result = subprocess.run(cmd, capture_output=True)
     print(result.stdout)
