5f5a7df799663adba4b191eca3d675faf3621fe2
John Yang <byjohnyang@gmail.com>
2024-12-09 23:52:06 -0800

    Release 2.1.7

--- swebench/__init__.py
+++ swebench/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "2.1.6"
+__version__ = "2.1.7"
 
 from swebench.collect.build_dataset import main as build_dataset
 from swebench.collect.get_tasks_pipeline import main as get_tasks_pipeline
